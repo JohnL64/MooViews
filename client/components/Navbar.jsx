717e351344e71dd533a7d1eaa8b85b65b6f55d6a
John Lee <jle432edu@gmail.com>
2021-08-02 18:38:06 -0700

    Testing git updates

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   // api key for movie api
   const apiKey = '4cb4593eaec1b72711462d3b62f0196b'
   // 
-  const [searchInput, setSearchInput] = useState('');
+  // const [searchInput, setSearchInput] = useState('');
   const [searchResult, setSearchResult] = useState(null);
   const [focused, setFocused] = useState(false);
   const [error, setError] = useState(null);
 
   const onSearch = (e) => {
-    setSearchInput(e.target.value);
-
+    // setSearchInput(e.target.value);
+    
       if (e.target.value !== '') {
       fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${e.target.value}`)
         .then(res => res.json())
@@ -31,7 +31,7 @@ const Navbar = () => {
     <nav className={css.navbar}>
       <Link className={css.navMooViews} to='home'><p className={css.moo}>Moo</p><p className={css.views}>Views</p></Link>
       <div className={css.searchBar} onFocus={(e) => setFocused(true)} onBlur={(e) => setFocused(false)}>
-          <input type='text' className={css.searchInput} value={searchInput} onChange={onSearch}></input>
+          <input type='text' className={css.searchInput} /*value={searchInput}*/ onChange={onSearch}></input>
           { (focused && searchResult) && <SearchResult searchResult={searchResult} /> }
           { error && <p>{error}</p>}
       </div>
--- client/components/SearchResult.jsx
+++ client/components/SearchResult.jsx
@@ -10,11 +10,13 @@ const SearchResult = ({ searchResult }) => {
   )
   const topResults = [];
   for (let i = 0; i < upToEight; i++) {
-    console.log('here', searchResult)
+    console.log(searchResult[i].release_date[01])
+    let year = searchResult[i].release_date.slice(0, 4);
+    let test = 'Testing to check something'
     topResults.push(
       <div className={css.result} key={searchResult[i].id}>
         <p>{searchResult[i].original_title}</p>
-        <p>{searchResult[i].release_date}</p>
+        <p className={css.date}>{searchResult[i].release_date}</p>
       </div>
     )
   }
