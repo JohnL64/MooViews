f44dda29e62d321e85ae39bc101b6c93c86cf4b9
John Lee <jle432edu@gmail.com>
2021-08-04 14:16:50 -0700

    Complete styling for navbar and change date date received from api

--- client/components/SearchResult.jsx
+++ client/components/SearchResult.jsx
@@ -10,9 +10,9 @@ const SearchResult = ({ searchResult }) => {
   )
   const topResults = [];
   for (let i = 0; i < upToEight; i++) {
-    console.log(searchResult[i].release_date[01])
-    let year = searchResult[i].release_date.slice(0, 4);
-    let test = 'Testing to check something'
+    // console.log(searchResult[i].release_date[01])
+    // let year = searchResult[i].release_date.slice(0, 4);
+    // let test = 'Testing to check something'
     topResults.push(
       <div className={css.result} key={searchResult[i].id}>
         <p>{searchResult[i].original_title}</p>
