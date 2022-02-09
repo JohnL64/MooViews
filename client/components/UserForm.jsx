ee66235fc3eb0a7f37dd6aea88145ed11a81f862
John Lee <johnclee64@gmail.com>
2022-02-09 01:49:14 -0800

    Add confirm password field and add styling for input field alerts
    -Update to UserForm that adds styling to the input field alerts and as well as adding a “confirm password” input field to render only when the user is creating an account.

--- client/components/UserForm.jsx
+++ client/components/UserForm.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import css from '../styles/LoginSignup.module.css';
 import { Link } from 'react-router-dom';
+import { IoAlertCircleSharp } from 'react-icons/io5';
 
 const UserForm = ({ action, setValidatedUser}) => {
-  // Using state to track possible errors when verifying or creating an account. If an error occurs this state will store the error and render it to the page
-  const [userError, setUserError] = useState('');
   // Using state to store user inputted email, username, and password to display changes to the correct input field and to send user data to server.
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('');
+  const [confirmPW, setConfirmPW] = useState('');
+  // Using state to track possible errors when verifying or creating an account. If an error occurs this state will store the error and render it to the page
+  const [userError, setUserError] = useState('');
 
   // Created variables and conditional statements to dynamically render the form title and the text inside the button
   let formTitle;
@@ -30,26 +32,27 @@ const UserForm = ({ action, setValidatedUser}) => {
   const validAccount = (e) => {
     e.preventDefault();
     // makes requests to the server to create or verify accounts. If succesful redirect to Movies page
-    fetch(`/user/${action}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify( { email, username, password })
-    })
-    .then(res => res.json())
-    .then(data => {
-      // if error occurs on the server side run code in catch method
-      if (data.errorType) throw new Error('Error', { cause: data.errorType });
-      console.log('Inside of userform after signing up!!!!', data);
-      setValidatedUser(true);
-      history.push(sessionStorage.getItem('lastPage'));
-    })
-    .catch(err => {
-      // if an err has occurred update userError to the given cause to be displayed
-      setUserError(err.cause);
-      // clear input fields when error occurs
-      setUsername('');
-      setPassword('');
-    })
+    if (action === 'verifyAccount' || (action === 'createAccount' && password === confirmPW)) {
+      fetch(`/user/${action}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { email, username, password })
+      })
+      .then(res => res.json())
+      .then(data => {
+        // if error occurs on the server side run code in catch method
+        if (data.errorType) throw new Error('Error', { cause: data.errorType });
+        setValidatedUser(true);
+        history.push(sessionStorage.getItem('lastPage'));
+      })
+      .catch(err => {
+        // if an err has occurred update userError to the given cause to be displayed
+        setUserError(err.cause);
+        // clear input fields when error occurs
+        setUsername('');
+        setPassword('');
+      })
+    } else setUserError('Passwords must match.')
   }
 
   return (
@@ -58,10 +61,11 @@ const UserForm = ({ action, setValidatedUser}) => {
       <form className={css.innerForm} onSubmit={validAccount}>
         { action === 'createAccount' && <input type='email' id="email" placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)} /> }
         <input type='text' id="username" required value={username} placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
-        <input type='password' id="password" required value={password} placeholder='Password'  onChange={(e) => setPassword(e.target.value)} />
+        <input className={css.password} type='password' id="password" required value={password} placeholder='Password'  onChange={(e) => setPassword(e.target.value)} />
+        { action === 'createAccount' && <input type='password' id="password" required value={confirmPW} placeholder='Confirm Password'  onChange={(e) => setConfirmPW(e.target.value)} /> }
+        { userError && <p className={css.userLSerror}><IoAlertCircleSharp/> {userError}</p>}
         <button id={css.submitBtn}>{submitButton}</button>
       </form>
-      { userError && <p>{userError}</p>}
       { action === 'verifyAccount' && <p className={css.loginSignupLink}>Dont have an account? <Link to='/signup'>Sign Up</Link></p> }
       { action === 'createAccount' && <p className={css.loginSignupLink}>Have an account? <Link to='/login'>Log In</Link></p> }
     </div>
